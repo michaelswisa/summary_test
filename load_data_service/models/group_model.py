5a610bfdb1a24760082f77be37eecf40b5fbde78
Michael Swisa <ms0504109182@gmail.com>
2024-12-18 15:28:13 +0200

    add model group

---
+++ load_data_service/models/group_model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+
+    events = relationship('Event', back_populates='group')
