9176ff9571939840bc7e9c11a1c0141729936747
Michael Swisa <ms0504109182@gmail.com>
2024-12-22 14:44:38 +0200

    Add new query for events by year and region in analysis_repo.py
    - Introduced a new function to retrieve event counts grouped by year and region, enhancing the analytics capabilities of the service.
    - Cleaned up the existing query structure for average casualties by region for better readability.
    - Ensured proper session management and query execution for improved performance.
    
    These changes expand the analytics service's functionality for event analysis across different regions and timeframes.

--- analytics_service/repository/analysis_repo.py
+++ analytics_service/repository/analysis_repo.py
@@ -3,6 +3,7 @@ from analytics_service.models import AttackType, Region, City, Country, Group
 from analytics_service.models.event_model import Event
 from sqlalchemy import func
 
+
 # query 1
 def get_top_attack_types(top_n=None):
     session = get_session()
@@ -28,10 +29,10 @@ def get_average_casualties_by_region(top_n=None):
             Region.name,
             (func.avg(Event.killed * 2 + Event.wounded)).label('average_score')
         ).join(City, Event.city_id == City.id) \
-         .join(Country, City.country_id == Country.id) \
-         .join(Region, Country.region_id == Region.id) \
-         .group_by(Region.name) \
-         .order_by(func.avg(Event.killed * 2 + Event.wounded).desc())
+            .join(Country, City.country_id == Country.id) \
+            .join(Region, Country.region_id == Region.id) \
+            .group_by(Region.name) \
+            .order_by(func.avg(Event.killed * 2 + Event.wounded).desc())
 
         if top_n:
             query = query.limit(top_n)
@@ -57,5 +58,21 @@ def get_top_groups_by_casualties(top_n=None):
     finally:
         session.close()
 
-if __name__ == '__main__':
-    print(get_average_casualties_by_region())
+
+# query 6
+def get_events_by_year_and_region():
+    session = get_session()
+    try:
+        return session.query(
+            Region.name.label('region_name'),
+            Event.year.label('year'),
+            func.count(Event.id).label('total_events')
+        ) \
+            .join(Country, Region.id == Country.region_id) \
+            .join(City, Country.id == City.country_id) \
+            .join(Event, City.id == Event.city_id) \
+            .group_by(Region.name, Event.year) \
+            .order_by(Region.name, Event.year) \
+            .all()
+    finally:
+        session.close()
