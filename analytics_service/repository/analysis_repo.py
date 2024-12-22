fe84681d477dfca20d7a2299d4a063690b87fdf6
Michael Swisa <ms0504109182@gmail.com>
2024-12-22 12:39:07 +0200

    Refactor analytics service by removing attack type blueprint registration and adding new query for top groups by casualties
    - Removed the registration of the attack type blueprint in app.py, simplifying the application setup.
    - Enhanced analysis_repo.py by adding a new function to retrieve top groups based on total casualties, improving data analysis capabilities.
    - Updated the import statements to include the Group model, ensuring all necessary models are available for queries.
    
    These changes streamline the application structure and expand the analytics service's functionality for casualty analysis.

--- analytics_service/repository/analysis_repo.py
+++ analytics_service/repository/analysis_repo.py
@@ -1,9 +1,9 @@
 from analytics_service.database_postgres.db_connection import get_session
-from analytics_service.models import AttackType, Region, City, Country
+from analytics_service.models import AttackType, Region, City, Country, Group
 from analytics_service.models.event_model import Event
 from sqlalchemy import func
 
-
+# query 1
 def get_top_attack_types(top_n=None):
     session = get_session()
     try:
@@ -20,6 +20,7 @@ def get_top_attack_types(top_n=None):
         session.close()
 
 
+# query 2
 def get_average_casualties_by_region(top_n=None):
     session = get_session()
     try:
@@ -39,5 +40,22 @@ def get_average_casualties_by_region(top_n=None):
     finally:
         session.close()
 
+
+# query 3
+def get_top_groups_by_casualties(top_n=None):
+    session = get_session()
+    try:
+        query = session.query(
+            Group.name,
+            (func.sum(Event.killed) * 2 + func.sum(Event.wounded)).label('total_casualties')
+        ).join(Event).group_by(Group.name).order_by(func.sum(Event.killed) * 2 + func.sum(Event.wounded).desc())
+
+        if top_n:
+            query = query.limit(top_n)
+
+        return query.all()
+    finally:
+        session.close()
+
 if __name__ == '__main__':
-    print(get_average_casualties_by_region(top_n=5))
+    print(get_average_casualties_by_region())
